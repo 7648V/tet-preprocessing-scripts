5e82c8a88ec33bda7914e611093be46fa6abf10a
7648V <131381372+7648V@users.noreply.github.com>
2023-04-20 23:24:32 +0100

    Add files via upload

---
+++ main_2.js
@@ -0,0 +1,69 @@
+//Creates CSV files containing the preprocessed data from the questionnaire data
+//One file is created per session for the TET pain trace data
+//One file is created per session for the pain type data
+
+const fs = require('fs')
+
+//CHANGE FILES ACCORDING TO THE PARTICIPANT BEING PREPROCESSED
+//files = ["01_Feb_23_P05","05_Dec_22_P05","07_Jan_23_P05","11_Dec_22_P05","12_Jan_23_P05","13_Dec_22_P05","14_Dec_22_P05","15_Dec_22_P05","18_Dec_22_P05","26_Jan_23_P05","30_Dec_22_P05"]
+//files = ["01_Dec_22_P01","04_Dec_22_P01","06_Dec_22_P01","07_Dec_22_P01","08_Dec_22_P01","09_Dec_22_P01","12_Dec_22_P01","13_Dec_22_P01","14_Dec_22_P01","16_Nov_22_P01","17_Nov_22_P01","18_Nov_22_P01","19_Nov_22_P01","21_Nov_22_P01","22_Nov_22_P01","23_Nov_22_P01","24_Nov_22_P01","25_Nov_22_P01","29_Nov_22_P01","30_Nov_22_P01"]
+files = ["01_Mar_23_P06","03_Mar_23_P06","20_Feb_23_P06","26_Feb_23_P06","27_Feb_23_P06","28_Feb_23_P06"]
+
+for (let i = 0; i < files.length; i++) {
+    //Creates a csv file for the TET coordinate data of participant P01 for a certain date
+    const data = require("/Users/pranjalpandey/Documents/Year 3/Project/TET Data/P06/"+files[i]+".json");
+    //Indexes in the JSON files corresponding to the TET pain data for conditions 1, 2, 3, 4
+    conditions = [7, 18, 29, 40]
+    output = ""
+
+    for (let condition of conditions) {
+        //Finds the condition names so that the randomised order of the conditions can be unrandomised
+        if (condition == 7) {
+            conditionname = JSON.stringify(data[3].stimulus)
+          } else if (condition == 18) {
+            conditionname = JSON.stringify(data[14].stimulus)
+        } else if (condition == 29) {
+            conditionname = JSON.stringify(data[25].stimulus)
+          } else {
+            conditionname = JSON.stringify(data[36].stimulus)
+          }
+        output += conditionname + "\n"
+
+        //Adds the Y coordinates of the pain TET traces to the CSV file output
+        for (let coordinate of data[condition].strokes[data[condition].strokes.length - 1]) {
+            if (coordinate.x !== undefined) {
+                output += coordinate.x + "," + coordinate.y + "\n"
+            }
+        }
+
+
+    }
+    //Writes output to the CSV file
+    fs.writeFile(files[i]+"_TET"+'.csv', output, 'utf8', (err) => console.error(err))
+
+    console.log(output)
+
+    conditions = [6, 17, 28, 39]
+    output = ""
+    for (let condition of conditions) {
+        for (let condition of conditions) {
+          //Finds condition names (same as above)
+            if (condition == 6) {
+                conditionname = JSON.stringify(data[3].stimulus)
+              } else if (condition == 17) {
+                conditionname = JSON.stringify(data[14].stimulus)
+            } else if (condition == 28) {
+                conditionname = JSON.stringify(data[25].stimulus)
+              } else {
+                conditionname = JSON.stringify(data[36].stimulus)
+              }
+            output += conditionname + "\n"
+            //Adds the pain types to the CSV file output
+        output += data[condition].response.pain.join(',')+"\n"
+
+    }
+    //Writes output to the CSV file
+    fs.writeFile(files[i]+"_TYPE"+'.csv', output, 'utf8', (err) => console.error(err))
+    console.log(output)
+}
+}
---
+++ realign.js
@@ -0,0 +1,54 @@
+//This code helps to realign the TET and EEG data by automatically outputting when each of the conditions start and end in Unix time for the cut20minutes function
+
+files = ["01_Mar_23_P06","03_Mar_23_P06","20_Feb_23_P06","26_Feb_23_P06","27_Feb_23_P06","28_Feb_23_P06"]
+for (let i = 0; i < files.length; i++) {
+    const data = require("/Users/pranjalpandey/Documents/Year 3/Project/TET Data/P06/"+files[i]+".json");
+    output = ""
+    output += files[i] + "\n" + "\n"
+
+    //Divide by 4000 to get to epochs - it's in miliseconds
+    var task_start = data[0].time_elapsed
+    task_start = task_start /=4000
+
+    var COND1_start = data[4].time_elapsed
+    COND1_start /=4000
+    COND1_start = COND1_start - task_start
+    var COND1_end = COND1_start + 75
+
+    var COND2_start = data[14].time_elapsed
+    COND2_start /=4000
+    COND2_start = COND2_start - task_start
+    var COND2_end = COND2_start + 75
+
+    var COND3_start = data[26].time_elapsed
+    COND3_start /=4000
+    COND3_start = COND3_start - task_start
+    var COND3_end = COND3_start + 75
+
+
+    var COND4_start = data[37].time_elapsed
+    COND4_start /=4000
+    COND4_start = COND4_start - task_start
+    var COND4_end = COND4_start + 75
+
+
+    output += "Order of the conditions:" + "\n" + "CONDITION 1: " + JSON.stringify(data[3].stimulus) + "\n" + "CONDITION 2: " + JSON.stringify(data[14].stimulus) + "\n" + "CONDITION 3: " +JSON.stringify(data[25].stimulus) + "\n" + "CONDITION 4: " + JSON.stringify(data[36].stimulus) + "\n" + "\n"
+
+    output += "Condition 1 start epoch:" + COND1_start + "\n"
+    output += "Condition 1 end epoch:" + COND1_end + "\n"
+    output += "\n"
+
+    output += "Condition 2 start epoch:" + COND2_start + "\n"
+    output += "Condition 2 end epoch:" + COND2_end + "\n"
+    output += "\n"
+    
+    output += "Condition 3 start epoch:" + COND3_start + "\n"
+    output += "Condition 3 end epoch:" + COND3_end + "\n"
+    output += "\n"
+
+    output += "Condition 4 start epoch:" + COND4_start + "\n"
+    output += "Condition 4 end epoch:" + COND4_end + "\n"
+    output += "\n"
+
+    console.log(output)
+}
